5b11f32bd17b5728add0e47a6d5467a8aa7a94b7
bala-amypo <bala@amypo.in>
2025-12-16 10:19:35 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/VisitLog.java
@@ -0,0 +1,50 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+
+public class VisitLog {
+    private long id;
+    private LocalDateTime entryTime;
+    private LocalDateTime exitTime;
+    private String purpose;
+    private String location;
+    public VisitLog(long id, LocalDateTime entryTime, LocalDateTime exitTime, String purpose, String location) {
+        this.id = id;
+        this.entryTime = entryTime;
+        this.exitTime = exitTime;
+        this.purpose = purpose;
+        this.location = location;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public LocalDateTime getEntryTime() {
+        return entryTime;
+    }
+    public void setEntryTime(LocalDateTime entryTime) {
+        this.entryTime = entryTime;
+    }
+    public LocalDateTime getExitTime() {
+        return exitTime;
+    }
+    public void setExitTime(LocalDateTime exitTime) {
+        this.exitTime = exitTime;
+    }
+    public String getPurpose() {
+        return purpose;
+    }
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
+
+}
