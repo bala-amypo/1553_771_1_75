e5015a06afd99a65a458f0b690b1f3e7c8584314
bala-amypo <bala@amypo.in>
2025-12-17 09:57:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/RiskScoreRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.RiskScore;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RiskScoreRepository
+        extends JpaRepository<RiskScore, Long> {
+
+    RiskScore findByVisitorId(Long visitorId);
+}
