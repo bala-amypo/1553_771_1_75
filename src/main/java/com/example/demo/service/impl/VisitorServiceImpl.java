4f664745e4f75246f35cd7b24065ff0e6b99b894
bala-amypo <bala@amypo.in>
2025-12-18 09:24:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VisitorServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VisitorServiceImpl.java
@@ -5,9 +5,11 @@ import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Visitor;
 import com.example.demo.repository.VisitorRepository;
 import com.example.demo.service.VisitorService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class VisitorServiceImpl implements VisitorService {
 
     private final VisitorRepository repository;
