47de768c1855ca85d65a743957fdd24184ab0e0b
bala-amypo <bala@amypo.in>
2025-12-18 08:44:22 +0000

    User commit

--- src/main/java/com/example/demo/controller/VisitorController.java
+++ src/main/java/com/example/demo/controller/VisitorController.java
@@ -2,34 +2,32 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Visitor;
 import com.example.demo.service.VisitorService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/visitors")
-@Tag(name = "Visitors")
 public class VisitorController {
 
-    private final VisitorService visitorService;
+    private final VisitorService service;
 
-    public VisitorController(VisitorService visitorService) {
-        this.visitorService = visitorService;
+    public VisitorController(VisitorService service) {
+        this.service = service;
     }
 
     @PostMapping
     public Visitor createVisitor(@RequestBody Visitor visitor) {
-        return visitorService.createVisitor(visitor);
+        return service.createVisitor(visitor);
     }
 
     @GetMapping("/{id}")
     public Visitor getVisitor(@PathVariable Long id) {
-        return visitorService.getVisitor(id);
+        return service.getVisitor(id);
     }
 
     @GetMapping
     public List<Visitor> getAllVisitors() {
-        return visitorService.getAllVisitors();
+        return service.getAllVisitors();
     }
 }
