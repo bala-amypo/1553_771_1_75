3be0792f7a523b76133dc1b7b04276ef7e0fd125
bala-amypo <bala@amypo.in>
2025-12-19 04:04:53 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RiskScoreServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RiskScoreServiceImpl.java
@@ -10,7 +10,7 @@ import com.example.demo.util.RiskLevelUtils;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
+@Service
 public class RiskScoreServiceImpl implements RiskScoreService {
 
     private final RiskScoreRepository riskScoreRepository;
